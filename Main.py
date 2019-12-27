52d0c6d8db1897e7b857654afb500541706dd474
Arjav Patel <33484527+Arjav-Patel@users.noreply.github.com>
2019-12-26 23:22:45 -0500

    Added context to frontend (#4)

--- Main.py
+++ Main.py
@@ -24,7 +24,7 @@ def makeLibrary():
     library.AddDict("jobs", jobs)
     return library
 
-def makeCharacter(library, i):
+def makeCharacter(server, i):
     stats = {}
     stats['STR'] = 8
     stats['DEX'] = 8 + 6
@@ -33,60 +33,42 @@ def makeCharacter(library, i):
     stats['WIS'] = 8 + 4
     stats['CHR'] = 8 + 6
 
-    job_names = library.GetNameUUIDAsDict("jobs")
-    
-    character = Character("Ace" + str(i), job_names["Card Dealer"], 20, 'M', "Neutral Good", stats)
-    character.AddToInventory('Sword', 2)
-    character.AddToInventory('Shield', 4)
-    character.AddToInventory('Helmet', 3)
-    character.AddToInventory('Boots', 8)
-    character.AddToInventory('Gloves', 3)
-    character.AddToInventory('Pants', 5)
-    character.AddToInventory('Shirt', 2)
-    character.AddToInventory('Apple', 6)
-    character.AddToInventory('Brick', 34)
+    job_names = server._library.GetNameUUIDAsDict("jobs")
+    server.AddCharacter("Ace" + str(i), job_names["Card Dealer"], 20, 'M', "Neutral Good", stats, 99, 11)
+    server.AddToInventory('Ace' + str(i), 'Sword', 2)
+    server.AddToInventory('Ace' + str(i), 'hield', 4)
+    server.AddToInventory('Ace' + str(i), 'elmet', 3)
+    server.AddToInventory('Ace' + str(i), 'Boots', 8)
+    server.AddToInventory('Ace' + str(i), 'loves', 3)
+    server.AddToInventory('Ace' + str(i), 'Pants', 5)
+    server.AddToInventory('Ace' + str(i), 'Shirt', 2)
+    server.AddToInventory('Ace' + str(i), 'Apple', 6)
+    server.AddToInventory('Ace' + str(i), 'rick', 34)
 
-    character.Equip('Sword')
-    character.Equip('Shield')
-    character.Equip('Helmet')
-    character.Equip('Boots')
-    character.Equip('Gloves')
-    character.Equip('Pants')
-    character.Equip('Shirt')
-
-    for _ in range(3): 
-        next_level_options = character.GetNextLevelOptions(library)
-
-        selected_options = {}
-        for option_uuid in next_level_options:
-            selected_options[option_uuid] = []
-            option_dict = next_level_options[option_uuid]
-            for ii in range(option_dict['num_options']):
-                selected_options[option_uuid].append(option_dict['feature_uuids'][ii])
-
-        character.IncrementLevel(library, selected_options)
-    character._learned_features = [''.join([c for c in str(feature) if c.isalpha()])[:6] for feature in character._learned_features]
-    
-    return character
-
-library = makeLibrary()
-characters = [makeCharacter(library, i) for i in range(3)]
-character = characters[0]
+    server.Equip('Ace' + str(i), 'Sword', 1)
+    server.Equip('Ace' + str(i), 'hield', 1)
+    server.Equip('Ace' + str(i), 'elmet', 1)
+    server.Equip('Ace' + str(i), 'Boots', 1)
+    server.Equip('Ace' + str(i), 'loves', 1)
+    server.Equip('Ace' + str(i), 'Pants', 1)
+    server.Equip('Ace' + str(i), 'Shirt', 1)
 
 from BackendServer import BackendServer
 
 server = BackendServer()
+for i in range(3):
+    makeCharacter(server, i)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', characters=characters, character=character)
+    return render_template('index.html', context=server.GetContext())
 
 @app.route('/import', methods=['GET', 'PUT'])
 def importFile():
     filename = request.args.get("importfile")
     dict_type = request.args.get("type")
 
-    server.ImportToLibrary(dict_type, filename):
+    server.ImportToLibrary(dict_type, filename)
     return render_template('index.html', context=server.GetContext())
 
 
