934638462bbb5d22b490934c97c875e2da9750c2
Swapnil Patel <swapnilpatel95@gmail.com>
2020-01-14 22:00:42 -0500

    Library class tests all written, all pass

--- Library.py
+++ Library.py
@@ -13,45 +13,120 @@ class Library(object):
         """
         self._dicts = dicts # dict of dicts
 
-    def AddToDict(self, dict_name, entries):
-        """
-        Add new entries to a dict in the library
-
-        Parameters:
-            dict_name (string): must be a valid dict name
-            entries (dict of uuid:object): new key/value pairs to add to specified dictionary. Validity
-                of values must be ensured by caller 
-        """
+    def AddToDict(self, dict_name, entries, mode = 'normal'):
+        """Add new entries to a dict in the library
+        
+        Arguments:
+            dict_name {string} -- must be a valid dict name
+            entries {dict of uuid:object} -- new key/value pairs to add to specified dictionary. Validity of values must be ensured by caller
+        
+        Returns:
+            bool -- Depends on Mode: 'normal' mode adds all non duplicate entries (of existing keys), true if no exceptions are thrown
+                                     'overwrite' mode adds all entries, overwriting any already existing keys, true if no exceptions are thrown
+                                     'safe' mode only updates if all entries are non duplicate of existing keys, true if entries added
+        """        
         if not (dict_name in self._dicts):
             print("Can't add new entries to dict <{}> because it isn't in the library".format(dict_name))
             return False
 
-        self._dicts[dict_name].update(entries)
-        return True
+        # If mode is overwrite, don't worry about existing keys, just overwrite them with the new entries
+        if mode == 'overwrite':
+            try:
+                self._dicts[dict_name].update(entries)
+                return True
+            except Exception as e:
+                print("Caught exception while tried to update dict <{}> with entries <{}>, got <{}>".format(dict_name, entries, e))
+                return False
 
-    def SetValidDicts(self, names):
-        """
-        This will delete any items in the library, and create empty dicts with the specified names
-        """
-        self._dicts.clear()
-        
-        for name in names:
-            self._dicts[name] = {}
-        
-    def AddNewValidDict(self, name):
-        if name in self._dicts.keys():
-            print("Can't add new dict <{}> to library, already exists".format(name))
+        # If mode is normal, go through each key and make sure every key it's new before making a decision
+        elif mode == 'normal':
+            try:
+                entries_to_add = {}
+                for key in entries:
+                    if key in self._dicts[dict_name]:
+                        print("Got existing key in AddToDict <{}>, will skip".format(key))
+                        continue
+                    else:
+                        entries_to_add[key] = entries[key]
+                self._dicts[dict_name].update(entries_to_add) # add only the vetted entries
+                return True
+            except Exception as e:
+                print("Caught exception while tried to update dict <{}> with entries <{}>, got <{}>".format(dict_name, entries, e))
+                return False
+            
+        # If mode is safe, then every entry is new, or reject all of them
+        elif mode == 'safe':
+            try:
+                for key in entries:
+                    if key in self._dicts[dict_name]:
+                        print("Got duplicate key in AddToDict with mode set to safe <{}>".format(key))
+                        return False
+                self._dicts[dict_name].update(entries) # all entries were good, add em
+            except Exception as e:
+                print("Caught exception while tried to update dict <{}> with entries <{}>, got <{}>".format(dict_name, entries, e))
+                return False
+
+        else:
+            print("Got invalid mode <{}> in AddToDict".format(mode))
             return False
 
-        self._dicts[name] = {}
+    def SetValidDicts(self, names):
+        """This will delete any items in the library, and create empty dicts with the specified names
+        
+        Arguments:
+            names {list of string} -- names of all dicts which will be valid in this dict library
 
-    def Peek(self, dict_name, this_uuid):
+        Returns:
+            bool -- True if successfully updated, false otherwise
+        """
+        try:
+            new_dicts = {}
+            for name in names:
+                new_dicts[name] = {}
+        except Exception as e:
+            print("Caught exception while trying to set new valid dicts <{}> for library, got <{}>".format(names, e))
+            return False
+
+        self._dicts.clear()
+        self._dicts = new_dicts
+        return True
+        
+    def AddNewValidDict(self, name):
+        """Adds a new dict name to the allowed dicts in the library
+        
+        Arguments:
+            name {string} -- name of the new dict
+        
+        Returns:
+            bool -- True if name already exists or successfully added, false otherwise
+        """
+        if name in self._dicts.keys():
+            print("Can't add new dict <{}> to library, already exists".format(name))
+            return True
+
+        try:
+            self._dicts[name] = {}
+            return True
+        except ValueError as e:
+            print("Failed to add new valid dict with name <{}>, got <{}>".format(name, e))
+            return False
+
+    def IsInDict(self, dict_name, this_uuid):
         """
         Returns true if dict_name exists and this_uuid exists in that dict
         """
         return (this_uuid in self.GetDict(dict_name))
 
     def Get(self, dict_name, this_uuid):
+        """Retrieves the specified object from the library
+        
+        Arguments:
+            dict_name {string} -- name of the dictionary to look into
+            this_uuid {uuid} -- uuid of the object to retrieve
+        
+        Returns:
+            Object or None -- object if found, None if not
+        """
         try:
             return self.GetDict(dict_name)[this_uuid]
         except KeyError:
--- tests/library_test.py
+++ tests/library_test.py
@@ -1,38 +1,160 @@
 from Library import Library
 import uuid
 import unittest
+from Effect import Effect
 from collections import Counter
 from pprint import pprint
+import numpy as np
 
 class LibraryTests(unittest.TestCase):
 
     def setUp(self):
         self.library = Library()
+        self.valid_dicts = ['features', 'effects', 'options', 'jobs']
 
     def test_ValidDicts(self):
-        valid_library_dicts = ['features', 'effects', 'options', 'jobs']
-        
         # Check that Set and Get ValidDict work
-        self.library.SetValidDicts(valid_library_dicts)
-        self.assertTrue(Counter(valid_library_dicts) == Counter(self.library.GetValidDicts()))
+        print("setting valid dicts\n")
+        self.library.SetValidDicts(self.valid_dicts)
+        self.assertTrue(Counter(self.valid_dicts) == Counter(self.library.GetValidDicts()))
         self.assertFalse(self.library.AddToDict('not_valid_dict', {uuid.uuid4():[], uuid.uuid4():[]}))
 
         # Add a new dict, check that old list doesn't match new list
+        print("adding new valid dict\n")
         self.library.AddNewValidDict("fancy")
-        self.assertFalse(Counter(valid_library_dicts) == Counter(self.library.GetValidDicts()))
-        self.assertTrue(Counter(valid_library_dicts + ['fancy']) == Counter(self.library.GetValidDicts()))
+        self.assertFalse(Counter(self.valid_dicts) == Counter(self.library.GetValidDicts()))
+        self.assertTrue(Counter(self.valid_dicts + ['fancy']) == Counter(self.library.GetValidDicts()))
 
         # Make sure adding to valid dict works
+        print("adding items to dict\n")
         self.assertTrue(self.library.AddToDict('fancy', {uuid.uuid4():[], uuid.uuid4():[]}))
         self.assertTrue(self.library.AddToDict('features', {uuid.uuid4():[]}))
         self.assertTrue(self.library.AddToDict('effects', {uuid.uuid4():[], uuid.uuid4():[], uuid.uuid4():[]}))
         self.assertTrue(self.library.AddToDict('effects', {}))
+        
+        print("ValidDicts test end\n")
 
-    def test_AddPeekGetDict(self):
-        pass
+    def test_Add_Get_IsInDict(self):
+        self.library.SetValidDicts(self.valid_dicts)
+
+        entries = {}
+        uuids = [uuid.uuid4() for _ in range(10)]
+        val = 0
+        for id in uuids:
+            entries[id] = val
+            val += 1
+ 
+        # Make sure adding entries works
+        print("Adding entries to dicts\n")
+        self.assertTrue(self.library.AddToDict('features', entries)) # should succed first time
+        self.assertTrue(self.library.AddToDict('features', {})) # empty entries dict should be fine
+        self.assertFalse(self.library.AddToDict('features', entries, mode='safe')) # should fail in safe mode
+        self.assertTrue(self.library.AddToDict('features', entries, mode='overwrite')) # should succeed in overwrite mode
+        self.assertTrue(self.library.AddToDict('features', entries, mode='normal')) # should succeed in normal mode
+        self.assertFalse(self.library.AddToDict('features', entries, mode='not_a_mode')) # should fail in invalid mode
+        self.assertFalse(self.library.AddToDict('not_valid', entries)) # invalid dict should be rejected
+        self.assertFalse(self.library.AddToDict('features', [3,4,56,6])) # entries should be a dict
+        self.assertFalse(self.library.AddToDict('features', 3)) # entries should be a dict
+        self.assertFalse(self.library.AddToDict('features', (3,6))) # entries should be a dict
+
+        # Make sure all added id's return true for IsInDict
+        print("Validating that entries are in dict\n")
+        for id in uuids:
+            self.assertTrue(self.library.IsInDict('features', id))
+
+        # Make sure random id's return false
+        print("Making sure random entries don't return true for IsInDict()\n")
+        for _ in range(10):
+            self.assertFalse(self.library.IsInDict('features', uuid.uuid4()))
+
+        # Make sure Library.Get(...) works
+        print("Testing Library.Get()\n")
+        for id in uuids:
+            print("<{}> , <{}>".format(entries[id], self.library.Get('features', id)))
+            self.assertTrue(entries[id] is self.library.Get('features', id))
+        print("\n")
+
+        # Create entries with uuid:tuple
+        entries = {}
+        uuids = [uuid.uuid4() for _ in range(10)]
+        vals = [ (np.random.randint(10), np.random.randint(10)) for _ in range(10)] #random tuples list
+        for ii in range(10):
+            entries[uuids[ii]] = vals[ii]
+
+        # Make sure we can add them to the dict
+        print("Adding tuple objects to a dict\n")
+        self.assertTrue(self.library.AddToDict('options', entries)) # uuid:tuple entries should add successfully
+
+        # Retrieve all the tuples and make sure they match
+        print("Making sure added tuples can be retrieved\n")
+        for id in uuids:
+            print("<{}> , <{}>".format(entries[id], self.library.Get('options', id)))
+            self.assertTrue(entries[id] is self.library.Get('options', id))
+        print("\n")
+
+        print("Library Add_Get_IsInDict test end\n")
 
     def test_GetNameUUID(self):
-        pass
+        self.library.SetValidDicts(self.valid_dicts)
+
+        # add some effects to the library
+        print("Adding some Effect instances to the library\n")
+        uuids = [uuid.uuid4() for _ in range(4)]
+        names = ['alpha', 'beta', 'gamma', 'delta']
+        effects = [
+            Effect(names[0], uuids[0], "attack", "instant", "alpha effect"),
+            Effect(names[1], uuids[1], "attack", "delayed", "beta effect"),
+            Effect(names[2], uuids[2], "attack", "persistent", "gamma effect"),
+            Effect(names[3], uuids[3], "attack", "reaction", "delta effect"),
+        ]
+        entries = {uuids[ii]:effects[ii] for ii in range(4)}
+        self.assertTrue(self.library.AddToDict('effects', entries))
+
+        # Retrieve and check effects dict
+        print("Testing GetDict from Library\n")
+        effects_dict = self.library.GetDict('effects')
+        print("Original entries\n")
+        pprint(entries)
+        print("\nRetrieved entries\n")
+        pprint(effects_dict)
+        self.assertTrue( len(effects_dict.keys()) == len(entries.keys()) ) # make sure the dict is the same size
+        for id, eff in effects_dict.items(): # make sure each item we added is accounted for    
+            self.assertTrue(id in entries.keys()) # make sure the key is in the dict
+            self.assertTrue(eff in entries.values()) # make sure the Effect object is in the dict
+
+        # Retrieve and check names
+        print("Testing GetNames from Library\n")
+        effect_names = self.library.GetNames('effects')
+        print("Original Effect Names <{}>".format(names))
+        print("Retrieved Effect Names <{}>\n".format(effect_names))
+        self.assertTrue( len(names) == len(effect_names) ) # make sure the dict is the same size
+        for name in names: # make sure all names match
+            self.assertTrue(name in effect_names)
+
+        # Retrieve and check uuids
+        print("Testing GetUUIDs from Library\n")
+        effect_uuids = self.library.GetUUIDs('effects')
+        print("Original UUIDs \n<{}>\n".format(uuids))
+        print("Retrieved UUIDs \n<{}>\n".format(effect_uuids))
+        self.assertTrue( len(uuids) == len(effect_uuids) ) # make sure the dict is the same size
+        for id in uuids: # make sure all names match
+            self.assertTrue(id in effect_uuids)
+
+        # Make sure getting uuid from name works
+        print("Testing GetUUIDFromName in Library\n")
+        for name in effect_names:
+            uuid_from_name = self.library.GetUUIDFromName('effects', name) # get uuid from name
+            effect_from_uuid = self.library.Get('effects', uuid_from_name) # get Effect instance using uuid
+            name_from_effect = effect_from_uuid.GetName() # get name from the Effect instance
+            self.assertTrue(name is name_from_effect) # make sure the names are the same
+
+        # Make sure the get name uuid dict is valid
+        print("Testing GetNameUUIDAsDict in Library\n")
+        name_uuid_dict = self.library.GetNameUUIDAsDict('effects')
+        for name, id in name_uuid_dict.items():
+            effect_from_uuid = self.library.Get('effects', id)
+            self.assertTrue(name is effect_from_uuid.GetName())
+
 
 if __name__=="__main__":
     unittest.main()
